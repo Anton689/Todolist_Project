dbf42a9e95b1722509108277d8b8d1608e414e83
Anton689 <zizugum@gmail.com>
2022-04-29 14:02:42 +0300

    Updates

---
+++ precache-manifest.82343a0e7c1debe70fa5dd351d1baaeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eebfdfa6a26ed4c421cb5a4e97b915d",
+    "url": "/Todolist_Project/index.html"
+  },
+  {
+    "revision": "41285361acc8620f0bc6",
+    "url": "/Todolist_Project/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "33172fd037951db3bf30",
+    "url": "/Todolist_Project/static/js/2.9f621716.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Todolist_Project/static/js/2.9f621716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41285361acc8620f0bc6",
+    "url": "/Todolist_Project/static/js/main.afb0352d.chunk.js"
+  },
+  {
+    "revision": "9ca8f1a807997f22d7a0",
+    "url": "/Todolist_Project/static/js/runtime-main.fded4266.js"
+  }
+]);
